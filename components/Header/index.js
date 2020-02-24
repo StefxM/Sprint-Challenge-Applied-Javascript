c71c67ef3d31e028429746a9b6cfa47fccfe5489
stephanie mairena <stefyem7711@gmail.com>
2020-02-23 19:42:27 -0500

    trying

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,26 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+    const divx = document.createElement('div');
+    divx.classList.add('header');
+
+    const spanx = document.createElement('span')
+    spanx.classList.add('date');
+    spanx.textContent = "March 28, 2019"
+    
+    const h1x = document.createElement('h1');
+    h1x.textContent = "Lambda Times"
+
+    const spanx2 = document.createElement('span');
+    spanx2.classList.add('temp');
+    spanx2.textContent = "98°"
+
+    .header-container.appendChild(divx);
+    .header-container.appendChild(spanx);
+    .header-container.appendChild(h1x);
+    .header-container.appendChild(spanx2);
+
+    return divx;
+}
+
